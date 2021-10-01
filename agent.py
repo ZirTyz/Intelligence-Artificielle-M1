3d0f4df8397827d7b3fe3b297e5b215f4f62dfa0
Lucas <lucas.miras@live.fr>
2021-10-01 17:41:56 -0400

    Add an heuristic to agent that found the best target

--- agent.py
+++ agent.py
@@ -1,3 +1,4 @@
+import random
 import time
 import states
 import House
@@ -10,6 +11,7 @@ class Agent:
         self.myPos = [0][0]
         self.dustPos = []
         self.jewelPos = []
+        self.dustJewelPos = []
         self.environment = _house
         self.state = states.State(self)
         self.target = None
@@ -26,28 +28,32 @@ class Agent:
     def ObserveEnvironment(self):
         self.dustPos = []
         self.jewelPos = []
+        self.dustJewelPos = []
         self.environment.m_rooms
         for x in range(0, len(self.environment.m_rooms)):
             for y in range(0, len(self.environment.m_rooms[x])):
                 room = self.environment.m_rooms[x][y]
-                if room.hasDust():
+                if room.getStateRoom() == 1:
                     self.dustPos.append([x, y])
-                if room.hasJewel():
+                if room.getStateRoom() == 2:
                     self.jewelPos.append([x, y])
+                if room.getStateRoom() == 3:
+                    self.dustJewelPos.append([x,y])
                 if room.hasRobot():
                     self.myPos = [x, y]
                 pass
         pass
 
     def UpdateMyState(self):
-        if self.jewelPos.__contains__(self.myPos):
-            self.state.sCurrentState("pick up")
-        elif self.dustPos.__contains__(self.myPos):
-            self.state.sCurrentState("vacuum")
-        elif len(self.dustPos) != 0:
-            self.state.sCurrentState("move")
-        else:
-            self.state.sCurrentState("idle")
+        # if self.jewelPos.__contains__(self.myPos):
+        #     self.state.sCurrentState("pick up")
+        # elif self.dustPos.__contains__(self.myPos):
+        #     self.state.sCurrentState("vacuum")
+        # elif len(self.dustPos) != 0:
+        #     self.state.sCurrentState("move")
+        # else:
+        #     self.state.sCurrentState("idle")
+        self.getAction()
 
 
     def live(self):
@@ -78,25 +84,52 @@ class Agent:
 
     def getScore(self, room):
         if room.getStateRoom() == 1: #Dust
-            return 6
+            return 8
         
         if room.getStateRoom() == 2: #Jewel
-            return 8
+            return 6
         
         if room.getStateRoom() == 3: #Dust and Jewel
             return 14
 
         if room.getStateRoom() == 4: #Nothing
             return 0
+    
     def getDistance(self, numH, numV ):
-        
         return abs(self.myPos[0] - numH) + abs(self.myPos[1] - numV)
+    
+    def getTarget(self):
+        priorities = []
+        weight = 0
+        for i in (self.dustJewelPos):
+            room = self.environment.m_rooms[i[0]][i[1]]
+            weight = self.getScore(room) - self.getDistance(room.getNumH(),room.getNumV())
+            priorities.append([room.getNumH(), room.getNumV(),weight])
 
-    def getAction(self, target): #target equal to pos of room durty
-        setProbabilities()
+        for i in (self.dustPos):
+            room = self.environment.m_rooms[i[0]][i[1]]
+            weight = self.getScore(room) - self.getDistance(room.getNumH(),room.getNumV())
+            priorities.append([room.getNumH(), room.getNumV(),weight])
+
+        for i in (self.jewelPos):
+            room = self.environment.m_rooms[i[0]][i[1]]
+            weight = self.getScore(room) - self.getDistance(room.getNumH(),room.getNumV())
+            priorities.append([room.getNumH(), room.getNumV(),weight])
+
+        if priorities.__len__() == 0:
+            self.target = None
+        else:
+            bestTarget = [priorities[0][0], priorities[0][1],priorities[0][2]]
+            for j in priorities:
+                if bestTarget[2] < j[2]:
+                    bestTarget = j
+
+            self.target = [bestTarget[0],bestTarget[1]]
+            
 
     def setProbabilities(self, target):
-        if target == 1 : #Dust if in position => vacuum near 100% else movement near 100%
+        targetState = target.getStateRoom()
+        if targetState == 1 : #Dust if in position => vacuum near 100% else movement near 100%
             if self.getDistance(self.target[0],self.target[1]) == 0:
                 self.probVacuum = 95
                 self.probPickUp = 5
@@ -106,7 +139,47 @@ class Agent:
                 self.probPickUp = 5
                 self.probMove = 90
 
-        #if target == 2
+        elif targetState == 2: #Jewel
+            if self.getDistance(self.target[0],self.target[1]) == 0:
+                self.probVacuum = 5
+                self.probPickUp = 95
+                self.probMove = 0
+            else:
+                self.probVacuum = 5
+                self.probPickUp = 5
+                self.probMove = 90
+
+        #Not totally usefull
+        elif targetState == 3: #Dust and Jewel
+            if self.getDistance(self.target[0],self.target[1]) == 0:
+                self.probVacuum = 50
+                self.probPickUp = 50
+                self.probMove = 0
+            else:
+                self.probVacuum = 5
+                self.probPickUp = 5
+                self.probMove = 90
         #
-        #
+
+    
+    def getAction(self): #target equal to pos of room "Durty"
+        self.getTarget()
+        if self.target == None:
+            self.state.sCurrentState("idle")
+        else:
+            target = self.environment.m_rooms[self.target[0]][self.target[1]]
+            self.setProbabilities(target)
+            proba = random.random() * 100
+            if proba <= self.probMove:
+                self.state.sCurrentState("move")
+            elif proba - self.probMove <= self.probPickUp:
+                self.state.sCurrentState("pick up")
+            elif proba - (self.probMove + self.probPickUp) <= self.probVacuum:
+                self.state.sCurrentState("vacuum")
+            else:
+                self.state.sCurrentState("idle")
+
+    def training(self):
+        return 1
+        
 
